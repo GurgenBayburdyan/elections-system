43eb1c1c2ffe8111a1ccd700c91463e07899338d
Arman Gevorgyan <39789090+gevorgyanarman@users.noreply.github.com>
2025-03-30 20:40:32 +0400

    Update Candidate.java

--- src/main/java/org/example/electionssystem/entity/Candidate.java
+++ src/main/java/org/example/electionssystem/entity/Candidate.java
@@ -13,14 +13,14 @@ import lombok.ToString;
 @Getter
 @ToString
 @Entity
-@Table(name = "candidate")
+@Table(name = "candidate") //todo please make uppercase
 public class Candidate {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "first_Name", nullable = false)
+    @Column(name = "first_Name", nullable = false) //todo please add length limit
     private String firstName;
 
     @Column(name = "last_Name", nullable = false)
